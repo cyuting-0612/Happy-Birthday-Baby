c65cfc5d06e58094cf89a4ca65040bc2efcd794c
cyt940612 <48167557+cyt940612@users.noreply.github.com>
2019-09-19 11:39:26 -0400

    Update script.js

--- static/js/script.js
+++ static/js/script.js
@@ -21,13 +21,13 @@ jQuery(document).ready(function($) {
     $(".right-paragraph p").attr('contenteditable', 'false');
     $("#username").attr('contenteditable', 'false');
     $("#persona-tag").attr('contenteditable', 'false');
-    $(".info-unit ul li").append('<span class="item-remove"><i class="iconfont icon-delete"></i></span>');
+  
     
     $(".info-unit").filter(function(index) {
         return ($(this).children('ul').length);
-    }).children("h2").append('<span class="item-add"><i class="iconfont icon-playlistadd"></i></span>');
+    }).children("h2").append('<span class="item-add"></span>');
 
-    $(".info-unit h2").append('<span class="unit-remove"><i class="iconfont icon-delete"></i></span>');
+    $(".info-unit h2").append('<span class="unit-remove"></span>');
     
 
     var portrait_modal = $('[data-remodal-id=portrait-modal]').remodal();
@@ -37,51 +37,18 @@ jQuery(document).ready(function($) {
     $('[data-remodal-id=portrait-modal] button').click(function(event) {
         $(".portrait").css('background-image', 'url("{0}")'.format($("#avatar-url").val()));
     });
-    var weixin_modal = $('[data-remodal-id=weixin-modal]').remodal();
-    $(".weixin").click(function(event) {
-        weixin_modal.open();
-    });
-    $('[data-remodal-id=weixin-modal] button').click(function(event) {
-        $(".weixin img").attr('src', $("#weixin-url").val());
-    });
 
-    $('.info-header').hover(function() {
-        /* Stuff to do when the mouse enters the element */
-        $(this).children('.unit-remove').css('visibility', 'visible');
-        $(this).children('.item-add').css('visibility', 'visible');
 
-    }, function() {
-        /* Stuff to do when the mouse leaves the element */
-        $(this).children('.unit-remove').css('visibility', 'hidden');
-        $(this).children('.item-add').css('visibility', 'hidden');
-    });
 
-    $('.info-unit ul li').hover(function() {
-        /* Stuff to do when the mouse enters the element */
-        $(this).children('.item-remove').css('visibility', 'visible');
 
-    }, function() {
-        /* Stuff to do when the mouse leaves the element */
-        $(this).children('.item-remove').css('visibility', 'hidden');
-    });
 
-    $('.unit-remove').click(function(event) {
-        $(this).closest(".info-unit").remove();
-    });
 
-    $('.item-remove').click(function(event) {
-        $(this).closest("li").remove();
-    });
 
-    $('.item-add').click(function(event) {
-        var unit = $(this).closest(".info-unit");
-        if (unit.children('ul')){
-            var list = unit.children('ul');
-            var clone = list.children('li:first-child').clone(true);
-            list.append(clone);
-        }
 
-    });
+
+
+
+ 
     $("progress").click(function(event) {
         $(this).attr("value", event.offsetX/$(this).width()/0.8 * $(this).attr("max"));
     });
